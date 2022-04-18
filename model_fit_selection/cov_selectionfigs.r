1859c3171aa1b7cb417a0c4279a9ef3f778e24e7
RoesM <markdroes@gmail.com>
2022-04-18 15:05:46 -0700

    Uploaded MIC data with resto index

--- model_fit_selection/cov_selectionfigs.r
+++ model_fit_selection/cov_selectionfigs.r
@@ -1,26 +1,25 @@
----
-title: "qrf_covariate_selection"
-author: "Mark Roes"
-date: "4/13/2022"
-output: html_document
----
+#Plots for covariate selection
+#Mark Roes
+#2022
+#-----------------------------------------------------------------
+# load/install libraries
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
+options(repos=structure(c(cran="https://ftp.osuosl.org/pub/cran/")))  
+packages <- c("tidyverse", "ggplot2", "lemon") 
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(packages, rownames(installed.packages())))  
+}  
+lapply(packages, require, character.only=TRUE)
 library(ggplot2)
 library(lemon)
 library(tidyverse)
-```
 
-```{r load data}
+
+#Load data------------------------------------------------------------
 data("MINE_stats")
-load("data/MINE_stats.rda")
-#mic_dat = rbind(mine_plot_list$Redds, mine_plot_list$Summer_CHaMP, mine_plot_list$Summer_DASH, mine_plot_list$Winter) %>%
-#select(species, MetricCategory, Metric, dataset, MIC, Name)
-  
-```
 
-```{r MIC chnkfig}
+#Species plots--------------------------------------------------------
+#Chinook
 mine_chnk_p = mine_plot_list %>%
   map(.f = function(x) {
     x %>%
@@ -52,10 +51,9 @@ for(i in 1:length(mine_chnk_p)) {
   print(mine_chnk_p[[i]])
 }
 dev.off()
-```
 
 
-```{r MIC stlhd fig}
+#Steelhead
 mine_stlhd_p = mine_plot_list %>%
   map(.f = function(x) {
     x %>%
@@ -88,12 +86,8 @@ for(i in 1:length(mine_stlhd_p)) {
 }
 dev.off()
 
-```
-
-
-Test MIC difference 
-
-```{r MIC SpeciesDiff}
+#MIC difference by species ------------------------------------------ 
+#Calculate differences
 stlhd = mine_plot_list %>%
   map(.f = function(x) {
     x %>%
@@ -109,15 +103,14 @@ chnk = mine_plot_list %>%
     }) %>%
   bind_rows()
   
-
-
 mic_diff = left_join(stlhd,chnk[,c(1:4,12)], by = c("MetricCategory","Metric","dataset")) %>%
   mutate(MIC_diff = abs(MIC.x - MIC.y)) %>%
   mutate(MIC_rel_diff = MIC.y/MIC.x) %>%
   split(.$dataset)
 
 
-mic_diff_p = mic_diff %>%
+#Generate figures
+mic_diff_s = mic_diff %>%
   map(.f = function(x) {
     x %>%
       mutate_at(vars(Metric, Name),
@@ -130,7 +123,6 @@ mic_diff_p = mic_diff %>%
                  y = MIC.x), alpha = 0.5)+
       facet_wrap(~MetricCategory, scales = 'free_y') +
       coord_flip() +
-      # scale_fill_viridis_d() +
       scale_fill_brewer(palette = 'Set3',
                         guide = guide_legend(nrow = 2)) +
       theme(legend.position = 'bottom',
@@ -138,18 +130,49 @@ mic_diff_p = mic_diff %>%
       labs(title = unique(x$dataset),
            fill = 'Category',
            x = element_blank(),
-           y = "Steelhead MIC and |MIC Difference with Chinook|)")
+           y = "Steelhead MIC and |MIC Difference with Chinook|")
   })
 
 
-pdf(paste0('MIC_diff.pdf'),
+mic_diff_c = mic_diff %>%
+  map(.f = function(x) {
+    x %>%
+      mutate_at(vars(Metric, Name),
+                list(~ fct_reorder(., .x = MIC.y))) %>%
+      arrange(desc(Metric)) %>%
+      ggplot() +
+      geom_col(aes(x = Name,
+                 y = MIC_diff)) +
+      geom_col(aes(x= Name,
+                 y = MIC.y), alpha = 0.5)+
+      facet_wrap(~MetricCategory, scales = 'free_y') +
+      coord_flip() +
+      scale_fill_brewer(palette = 'Set3',
+                        guide = guide_legend(nrow = 2)) +
+      theme(legend.position = 'bottom',
+            axis.text = element_text(size = 6)) +
+      labs(title = unique(x$dataset),
+           fill = 'Category',
+           x = element_blank(),
+           y = "Chinook MIC and |MIC Difference with Steelhead|")
+  })
+
+
+pdf(paste0('output/figures/MIC_diff.pdf'),
     width = 16,
     height = 9)
 
-for(i in 1:length(mic_diff_p)) {
-  print(mic_diff_p[[i]])
+for(i in 1:length(mic_diff_s)) {
+  print(mic_diff_s[[i]])
+  print(mic_diff_c[[i]])
 }
 
 dev.off()
 
-```
+#Flagging covs with restoration utility-------------------------------
+#Read in restoration info and bind to MIC data
+resto = read_csv("data/Resto_inform_cov.csv")[,-c(1)] ; names(resto)[names(resto)=="ShortName"] = "Metric"
+
+mic_dat = mic_diff %>%
+  bind_rows() %>%
+  left_join(select(resto, Metric, RestoInform) , by = c("Metric"))
